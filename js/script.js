6ee9840b8131fccfbdbe2c1907d4348e77d7e5ee
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-04-18 18:08:18 +0000

    Updated index.html Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
+/* global Phaser */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Created by: Jack LeVasseuer
+// Created on: April 17, 2024 
+// This is phaser3 game configuration file 
+
+/**
+* Start Phaser Game 
+*/
+const config = {
+  type: Phaser .AUTO,
+  width: 1920,
+  height: 1080, 
+  physics: {
+    default: "arcade" ,
+    arcade: {
+      debug: true, 
+    },
+  },
+  // set backround color 
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  }, 
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
